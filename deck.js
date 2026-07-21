18e8ef2c596d25081ebf95ff922551bd2bfe4542
policypal1 <samuelkumpula175@gmail.com>
2026-07-21 16:27:44 -0700

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzBw-XwcN0DjhyT_vcgbfwu3LrgqBNozlXW9DYo7vV9qt7sMKqxRVdz1egs-8yVZ0Q/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwD777f4p-FFlyUZLynIs69u7xod5TRHTJ7IUqIf_wFFRycV7tt4UwK0P1qbp9UDsju/exec";
 
 const MAX_IMAGE_FILES = 4;
 const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
